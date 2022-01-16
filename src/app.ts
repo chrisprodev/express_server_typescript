13105b9cc948d8e7434865a55bd3ac344780e4c8
Christian Lopez <chrisprodev@gmail.com>
2022-01-16 11:02:21 -0600

    Add dbConnection

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
+import config from "config";
+import connect from "./utils/dbConnection";
 
 const app = express();
-const port = 3000;
+const port = config.get<number>("port");
 
 app.get("/", (req, res) => {
   return res.send("Hello world");
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Server listening at port ${port}`);
+  await connect();
 });
